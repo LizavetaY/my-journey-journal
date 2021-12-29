7e7ef3c4ddf86922e230532cf2fd1cb0de664184
Lizaveta <liz07bokan@gmail.com>
2021-12-30 00:47:24 +0300

    Change links for pages and fonts for GitHub Pages

--- frontend/js/views/partials/header.js
+++ frontend/js/views/partials/header.js
@@ -8,9 +8,9 @@ class HeaderMain extends Component {
             resolve(`
                 <h1>My journey journal</h1>
                 <nav class="journalPages">
-                    <a class="header__link ${!resource ? 'active' : ''}" href="/#/">Home</a>
-                    <a class="header__link ${resource === 'trips' ? 'active' : ''}" href="/#/trips">Planned trips</a>
-                    <a class="header__link ${resource === 'about' ? 'active' : ''}" href="/#/about">About</a>
+                    <a class="header__link ${!resource ? 'active' : ''}" href="/my-journey-journal/#/">Home</a>
+                    <a class="header__link ${resource === 'trips' ? 'active' : ''}" href="/my-journey-journal/#/trips">Planned trips</a>
+                    <a class="header__link ${resource === 'about' ? 'active' : ''}" href="/my-journey-journal/#/about">About</a>
                 </nav>
                 <div id="dataToday"></div>
             `);
